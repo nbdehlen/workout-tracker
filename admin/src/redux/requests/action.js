1058fa7f3bac9decf1d1e79cff75f658f0e4ec68
Bastian <nbdehlen@gmail.com>
2020-12-13 20:33:04 +0100

    Add env base url heroku backend

--- admin/src/redux/requests/action.js
+++ admin/src/redux/requests/action.js
@@ -1,46 +1,46 @@
-import { FETCH_USERS, DELETE_USER, EDIT_USER, ADD_USER } from './actionTypes';
+import { FETCH_USERS, DELETE_USER, EDIT_USER, ADD_USER } from "./actionTypes";
 
 export const fetchUsers = (token) => ({
   type: FETCH_USERS,
   request: {
-    url: 'http://localhost:5000/api/v1/admin',
-    method: 'GET',
-    headers: { 'x-access-token': token },
+    url: `${process.env.REACT_APP_BASE_API_URL}/api/v1/admin`,
+    method: "GET",
+    headers: { "x-access-token": token },
   },
 });
 
 export const deleteUser = (token, id) => ({
   type: DELETE_USER,
   request: {
-    url: `http://localhost:5000/api/v1/admin/${id}`,
-    method: 'DELETE',
-    headers: { 'x-access-token': token },
+    url: `${process.env.REACT_APP_BASE_API_URL}/api/v1/admin/${id}`,
+    method: "DELETE",
+    headers: { "x-access-token": token },
   },
 });
 
 export const editUser = (token, id, username, email) => ({
   type: EDIT_USER,
   request: {
-    url: `http://localhost:5000/api/v1/admin/${id}`,
-    method: 'PATCH',
+    url: `${process.env.REACT_APP_BASE_API_URL}/api/v1/admin/${id}`,
+    method: "PATCH",
     data: {
       username: username,
       email: email,
     },
-    headers: { 'x-access-token': token },
+    headers: { "x-access-token": token },
   },
 });
 
 export const addUser = (token, username, email, password) => ({
   type: ADD_USER,
   request: {
-    url: 'http://localhost:5000/api/v1/admin/',
-    method: 'POST',
+    url: `${process.env.REACT_APP_BASE_API_URL}/api/v1/admin/`,
+    method: "POST",
     data: {
       username: username,
       email: email,
       password: password,
     },
-    headers: { 'x-access-token': token },
+    headers: { "x-access-token": token },
   },
 });
