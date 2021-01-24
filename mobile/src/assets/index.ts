cb4dabfa0f893a8e15d6a7e0fd0b83a7cb9fab47
Bastian <nbdehlen@gmail.com>
2021-01-24 17:34:37 +0100

    Add svgs for login screen

--- mobile/src/assets/index.ts
+++ mobile/src/assets/index.ts
@@ -2,10 +2,16 @@ import TrashCan from './svg/TrashCan.svg'
 import Settings from './svg/Settings.svg'
 import List from './svg/List.svg'
 import Add from './svg/Add.svg'
+import User from './svg/User.svg'
+import Email from './svg/Email.svg'
+import Lock from './svg/Lock.svg'
 
 export const Icons = {
   Add,
   List,
   Settings,
   TrashCan,
+  User,
+  Email,
+  Lock,
 }
