dcd5343d23906fd1fd6dd360198aa6aa9d88c475
Bastian <nbdehlen@gmail.com>
2021-01-28 06:22:12 +0100

    add icons to WorkoutList

--- mobile/src/assets/index.ts
+++ mobile/src/assets/index.ts
@@ -5,6 +5,11 @@ import Add from './svg/Add.svg'
 import User from './svg/User.svg'
 import Email from './svg/Email.svg'
 import Lock from './svg/Lock.svg'
+import Cycle from './svg/Bicycle.svg'
+import Run from './svg/Running.svg'
+import Gym from './svg/Gym.svg'
+import Sport from './svg/Sport.svg'
+import Muscle from './svg/Muscle.svg'
 
 export const Icons = {
   Add,
@@ -14,4 +19,9 @@ export const Icons = {
   User,
   Email,
   Lock,
+  Gym,
+  Run,
+  Cycle,
+  Sport,
+  Muscle,
 }
