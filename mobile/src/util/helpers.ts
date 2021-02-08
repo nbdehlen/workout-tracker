617649461c5afbebd03192116613ef32bc91099d
Bastian <nbdehlen@gmail.com>
2021-02-08 01:29:12 +0100

    Format array to text string function

--- mobile/src/util/helpers.ts
+++ mobile/src/util/helpers.ts
@@ -1,4 +1,4 @@
-export const ucFirst = (str) => {
+export const ucFirst = (str: string): string => {
   if (!str) {
     return str
   }
@@ -12,4 +12,28 @@ export const calculcateTotalSets = (workout) => {
     count += exercise.sets.length
   })
   return count
+}
+
+export const arrayTextFormat = (arr: string[], cutOff = 100): string => {
+  let returnStr = ''
+
+  arr.map((str, i) => {
+    if (returnStr.length + 3 <= cutOff && arr.length - 1 === i) {
+      returnStr += '...'
+    } else if (returnStr.length + str.length <= cutOff) {
+      if (
+        arr.length - 1 !== i &&
+        returnStr.length + str.length + arr[i + 1].length <= cutOff
+      ) {
+        returnStr += ucFirst(str) + ', '
+      } else if (
+        arr.length - 1 !== i &&
+        returnStr.length + str.length <= cutOff
+      ) {
+        returnStr += ucFirst(str)
+      }
+    }
+  })
+
+  return returnStr
 }
