506b56da095025915da336f5c60d214cc9567a44
Bastian <nbdehlen@gmail.com>
2021-02-10 11:08:03 +0100

    Switch baseUrl

--- admin/src/redux/store.js
+++ admin/src/redux/store.js
@@ -1,42 +1,37 @@
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from "redux"
 // import postReducer from '../reducers/postReducer';
-import authReducer from './auth/authReducer';
-import userReducer from './users/userReducer';
-import axios from 'axios';
-import { onRequest, onSuccess, onError } from './requests/interceptors';
-import { handleRequests } from '@redux-requests/core';
-import { createDriver } from '@redux-requests/axios';
+import authReducer from "./auth/authReducer"
+import userReducer from "./users/userReducer"
+import axios from "axios"
+import { onRequest, onSuccess, onError } from "./requests/interceptors"
+import { handleRequests } from "@redux-requests/core"
+import { createDriver } from "@redux-requests/axios"
 
 export const configureStore = () => {
   const { requestsReducer, requestsMiddleware } = handleRequests({
-    driver: createDriver(
-      axios,
-      // axios.create({
-      //   baseURL: 'http://localhost:5000',
-      // }),
-    ),
+    driver: createDriver(axios),
     onRequest,
     onSuccess,
     onError,
-  });
+  })
 
   const composeEnhancers =
-    (typeof window !== 'undefined' &&
+    (typeof window !== "undefined" &&
       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-    compose;
+    compose
 
   const reducers = combineReducers({
     user: userReducer,
     auth: authReducer,
     requests: requestsReducer,
-  });
+  })
 
   const store = createStore(
     reducers,
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    composeEnhancers(applyMiddleware(...requestsMiddleware)),
-  );
-  return store;
-};
+    composeEnhancers(applyMiddleware(...requestsMiddleware))
+  )
+  return store
+}
 
-export default configureStore;
+export default configureStore
--- mobile/src/api/constants.ts
+++ mobile/src/api/constants.ts
@@ -1,5 +1,5 @@
-// const baseUrl = 'https://workouttracker-api.herokuapp.com'
-const baseUrl = 'http://10.0.2.2:5000'
+const baseUrl = 'https://workouttracker-api.herokuapp.com'
+// const baseUrl = 'http://10.0.2.2:5000'
 
 // TODO: envs for dev/prod?
 
